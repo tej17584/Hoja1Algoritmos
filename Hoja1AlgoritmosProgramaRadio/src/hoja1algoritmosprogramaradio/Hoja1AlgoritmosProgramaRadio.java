ac2e85837f93ec5b42fef88fb9d7634ba00149e3
José Alejandro Tejada Flores <tej17584@uvg.edu.gt>
2018-01-12 11:28:19 -0600

    primer repositorio con cambios

---
+++ Hoja1AlgoritmosProgramaRadio/src/hoja1algoritmosprogramaradio/Hoja1AlgoritmosProgramaRadio.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoja1algoritmosprogramaradio;
+
+/**
+ *
+ * @author Jose Tejada
+ */
+public class Hoja1AlgoritmosProgramaRadio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int hola;
+    }
+    
+}
