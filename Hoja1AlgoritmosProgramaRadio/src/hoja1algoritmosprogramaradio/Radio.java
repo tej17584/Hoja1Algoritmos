2febf2f88f017a2867a0dbd14b770d6d03302326
SDiego <SDiego@SDiego-PC>
2018-01-16 13:17:50 -0600

    prueba 2

--- Hoja1AlgoritmosProgramaRadio/src/hoja1algoritmosprogramaradio/Radio.java
+++ Hoja1AlgoritmosProgramaRadio/src/hoja1algoritmosprogramaradio/Radio.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hoja1algoritmosprogramaradio;
 
 /**
@@ -11,6 +7,61 @@ package hoja1algoritmosprogramaradio;
  */
 public class Radio implements douglas {
     
+    private boolean emisora;
+    private boolean onoff;
+    private float numeracion;
+    
+    public Radio(){
+        
+    }
+    /**
+     * 
+     * @param e
+     * @return 
+     */
+    public boolean onOff(boolean e){
+    
+    }
+    
+    /**
+     * 
+     * @param e
+     * @return 
+     */
+    public boolean Switch(boolean e){
+    
+    }
+    /**
+     * 
+     * @param a
+     * @return 
+     */
+    public float siguiente(float a){
+    
+    }
+    /**
+     * 
+     * @param a
+     * @return 
+     */
+    public float anterior(float a){
+    
+    }
+    /**
+     * 
+     * @param e
+     * @param b 
+     */
+    public void guardar(float e,int b){
+    
+    }
+    /**
+     * 
+     * @param b 
+     */
+    public void SeleccionarFav(int b){
+    
+    }
     
     
     
